3d24cd0ed5ce80e6338898665a4c365ddd598338
MartinGrue <gruebler.martin@outlook.de>
2019-10-25 14:05:13 +0200

    added Identity + Bugfix appsettings

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,10 +1,11 @@
 ﻿
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
@@ -14,6 +15,7 @@ namespace Persistence
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
             builder.Entity<Value>().ToTable("Values")
             .HasData(
                 new Value() { Id = 1, Name = "Value01" },
