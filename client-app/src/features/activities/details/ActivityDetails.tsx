b95f0d15d558358fe34c7a90f38a6ccc423766d8
MartinGrue <gruebler.martin@outlook.de>
2020-01-04 20:52:29 +0100

    delete comment

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -29,8 +29,6 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
     window.scrollTo(0, 0);
   }, [match.params.id, loadActivity]);
 
-  // return <p>Details {match.params.id}</p>
-
   if (loadingInitial || !selectedActivity) {
     return <LoadingComponent content='Fetching Activity...'></LoadingComponent>;
   }
@@ -54,36 +52,6 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({
         ></ActivityDetailedSideBar>
       </GridColumn>
     </Grid>
-    // <Card fluid>
-    //   <Image
-    //     src={`/assets/categoryImages/${selectedActivity!.category}.jpg`}
-    //     wrapped
-    //     ui={false}
-    //   />
-    //   <Card.Content>
-    //     <Card.Header>{selectedActivity!.title}</Card.Header>
-    //     <Card.Meta>
-    //       <span className='date'>{selectedActivity!.date}</span>
-    //     </Card.Meta>
-    //     <Card.Description>{selectedActivity!.description}</Card.Description>
-    //   </Card.Content>
-    //   <Card.Content extra>
-    //     <Button.Group widths={2}>
-    //       <Button
-    //         basic
-    //         color='blue'
-    //         content='Edit'
-    //         as={Link} to={`/manage/${selectedActivity.id}`}
-    //       ></Button>
-    //       <Button
-    //         basic
-    //         color='grey'
-    //         content='Cancel'
-    //         as={Link} to='/activities'
-    //       ></Button>
-    //     </Button.Group>
-    //   </Card.Content>
-    // </Card>
   );
 };
 
