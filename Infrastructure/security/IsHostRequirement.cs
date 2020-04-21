3761fd0ea7787c52884cc295eac238757ccee9b2
MartinGrue <gruebler.martin@outlook.de>
2020-04-21 17:59:30 +0200

    BugFix for new asp net core routing

--- Infrastructure/security/IsHostRequirement.cs
+++ Infrastructure/security/IsHostRequirement.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
-using Application.interfaces;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
 using Persistence;
 
 namespace Infrastructure.security
@@ -13,34 +13,28 @@ namespace Infrastructure.security
     }
     public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
     {
-        private readonly IUserAccessor userAccessor;
-        private readonly DataContext datacontext;
-        public IsHostRequirementHandler(DataContext datacontext, IUserAccessor userAccessor)
+        private readonly DataContext context;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public IsHostRequirementHandler(IHttpContextAccessor httpContextAccessor, DataContext context)
         {
-            this.datacontext = datacontext;
-            this.userAccessor = userAccessor;
+            this.httpContextAccessor = httpContextAccessor;
+            this.context = context;
 
         }
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext authHandlercontext, IsHostRequirement requirement)
         {
-            if (context.Resource is AuthorizationFilterContext authContext)
-            {
-                var username = userAccessor.GetCurrentUsername();
-                var activityId = Guid.Parse(authContext.RouteData.Values["id"].ToString());
-                // var activityFromRepo = datacontext.Activities
-                // .SingleOrDefault(p => p.Id == activityId);
-                var activityFromRepo = datacontext.Activities.FindAsync(activityId).Result;
-                var host = activityFromRepo.UserActivities.FirstOrDefault(x => x.IsHost);
+            var username = httpContextAccessor.HttpContext.User?.Claims?.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            // var activityId = Guid.Parse(authContext.RouteData.Values["id"].ToString());
+            var activityId = Guid.Parse(httpContextAccessor.HttpContext.Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value.ToString());
 
-                if (host?.AppUser?.UserName == username)
-                {
-                    context.Succeed(requirement);
-                }
-                else
-                {
-                    context.Fail();
-                }
+            var activityFromRepo = context.Activities.FindAsync(activityId).Result;
+            var host = activityFromRepo.UserActivities.FirstOrDefault(x => x.IsHost);
+
+            if (host?.AppUser?.UserName == username)
+            {
+                authHandlercontext.Succeed(requirement);
             }
+
             return Task.CompletedTask;
         }
     }
