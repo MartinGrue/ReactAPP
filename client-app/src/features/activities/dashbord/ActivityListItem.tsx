b975fc2f487a2bcb192e1c80e1a3f45363e7a56e
MartinGrue <gruebler.martin@outlook.de>
2020-11-13 23:29:53 +0100

    fixed dashboard responsive issuse

--- client-app/src/features/activities/dashbord/ActivityListItem.tsx
+++ client-app/src/features/activities/dashbord/ActivityListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Item,
   Button,
@@ -8,77 +8,91 @@ import {
   ItemDescription,
   Grid,
   Header,
-  Divider
-} from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
-import { IActivity } from '../../../app/models/IActivity';
-import { observer } from 'mobx-react-lite';
-import { format } from 'date-fns';
-import ActivityListAttendee from './ActivityListAttendee';
-import { SimpleMap } from '../../../app/common/maps/SimpleMap';
+  Divider,
+  GridColumn,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { IActivity } from "../../../app/models/IActivity";
+import { observer } from "mobx-react-lite";
+import { format } from "date-fns";
+import ActivityListAttendee from "./ActivityListAttendee";
+import { SimpleMap } from "../../../app/common/maps/SimpleMap";
 
 const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
-  const host = activity.userActivities.filter(x => x.isHost)[0];
+  const host = activity.userActivities.filter((x) => x.isHost)[0];
   return (
     <Segment.Group>
       <Segment>
-      <Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Header>
-      <Divider></Divider>
+        <Header as={Link} to={`/activities/${activity.id}`}>
+          {activity.title}
+        </Header>
+        <Divider></Divider>
         <Grid stackable>
-          <Grid.Column width={8}>
-            <Item.Group>
-              <Item key={activity.id}>
-                <Item.Image
-                  size='tiny'
-                  circular
-                  src={host.image || '/assets/user.png'}
-                  style={{ marginBottom: 3 }}
-                  as={Link}
-                  to={`/profiles/${host.userName}`}
-                ></Item.Image>
-                <Item.Content>
-              
-                  <Item.Description>
-                    <Header> Hosted by {' '}
-                    <Link to={`/profiles/${host.userName}`}>
-                      {host.displayName}
-                    </Link></Header>
-                   
-                  </Item.Description>
-                  {activity.isHost && (
-                    <ItemDescription>
-                      <Label
-                        tag
-                        color='orange'
-                        content='you are hosting this activity'
-                      ></Label>
-                    </ItemDescription>
-                  )}
-                  {activity.isGoing && !activity.isHost && (
-                    <ItemDescription>
-                      <Label
-                        tag
-                        color='green'
-                        content='you are going to this activity'
-                      ></Label>
-                    </ItemDescription>
-                  )}
-                </Item.Content>
-              </Item>
-            </Item.Group>
+          <Grid.Column width={8}  verticalAlign="middle">
+            <Grid>
+              <Grid.Column width={8} verticalAlign="middle">
+                <Item.Group>
+                  <Item key={activity.id}>
+                    <Item.Image
+                      size="big"
+                      circular
+                      src={host.image || "/assets/user.png"}
+                      style={{ marginBottom: 3 }}
+                      as={Link}
+                      to={`/profiles/${host.userName}`}
+                    ></Item.Image>
+                  </Item>
+                </Item.Group>
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <Item.Group>
+                  <Item key={activity.id}>
+                    <Item.Content>
+                      <Item.Description>
+                        <Header>
+                          Hosted by{" "}
+                          <Link to={`/profiles/${host.userName}`}>
+                            {host.displayName}
+                          </Link>
+                        </Header>
+                      </Item.Description>
+                      {activity.isHost && (
+                        <ItemDescription>
+                          <Label
+                            tag
+                            color="orange"
+                            content="you are hosting this activity"
+                          ></Label>
+                        </ItemDescription>
+                      )}
+                      {activity.isGoing && !activity.isHost && (
+                        <ItemDescription>
+                          <Label
+                            tag
+                            color="green"
+                            content="you are going to this activity"
+                          ></Label>
+                        </ItemDescription>
+                      )}
+                    </Item.Content>
+                  </Item>
+                </Item.Group>
+              </Grid.Column>
+            </Grid>
           </Grid.Column>
           <Grid.Column width={8}>
             <SimpleMap
-            lat={activity.latitute} lng={activity.longitute}
-              opt={{ style: { width: '100%', height: 200 } }}
+              lat={activity.latitute}
+              lng={activity.longitute}
+              opt={{ style: { width: "100%", height: 200 } }}
             ></SimpleMap>
           </Grid.Column>
         </Grid>
       </Segment>
       <Segment>
-        <Icon name='clock' />
-        {format(activity.date!, 'HH:mm')}
-        <Icon name='marker' />
+        <Icon name="clock" />
+        {format(activity.date!, "HH:mm")}
+        <Icon name="marker" />
         {activity.venue},{activity.city}
       </Segment>
       <Segment secondary>
@@ -89,9 +103,9 @@ const ActivityListItem: React.FC<{ activity: IActivity }> = ({ activity }) => {
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
-          floated='right'
-          content='View'
-          color='blue'
+          floated="right"
+          content="View"
+          color="blue"
           as={Link}
           to={`/activities/${activity.id}`}
         ></Button>
