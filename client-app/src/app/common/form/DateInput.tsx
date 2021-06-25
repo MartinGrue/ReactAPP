800308b87096e0e661b3a5d67bb781f26f23296d
MartinGrue <gruebler.martin@outlook.de>
2021-06-25 18:36:11 +0200

    fixed localisation

--- client-app/src/app/common/form/DateInput.tsx
+++ client-app/src/app/common/form/DateInput.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { FieldRenderProps } from "react-final-form";
 import { FormFieldProps, Form, Label } from "semantic-ui-react";
-import { DateTimePicker } from "react-widgets";
-
-interface IProps
-  extends FieldRenderProps<Date, HTMLElement>,
-    FormFieldProps {}
+import { DateTimePicker, FormatterOverrides } from "react-widgets";
+import { Localization } from "react-widgets";
+import { DateLocalizer, NumberLocalizer } from "react-widgets/IntlLocalizer";
+interface IProps extends FieldRenderProps<Date, HTMLElement>, FormFieldProps {}
 
 const DateInput: React.FC<IProps> = ({
   input,
@@ -17,28 +16,29 @@ const DateInput: React.FC<IProps> = ({
   ...rest
 }) => {
   return (
-    <Form.Field error={touched && !!error} width={width}>
-      {/* <DateTimePicker
-        containerClassName={"dateTimePicker-Container"}
-        onKeyDown={(e) => e.preventDefault()}
-        onBlur={input.onBlur}
-        // time={time}
-        // timeFormat="HH:mm"
-        step={15}
-        // date={date}
-        // format={date ? "dd.MM.yyyy" : "HH:mm"}
-        onChange={input.onChange}
-        value={input.value || null}
-        placeholder={placeholder}
-        {...rest}
-        id="id"
-      /> */}
-      {touched && !!error && (
-        <Label basic color="red">
-          {error}
-        </Label>
-      )}
-    </Form.Field>
+    <Localization
+      date={new DateLocalizer({ culture: "de-de", firstOfWeek: 1 })}
+    >
+      <Form.Field error={touched && !!error} width={width}>
+        <DateTimePicker
+          // containerClassName={"dateTimePicker-Container"}
+          onKeyDown={(e) => e.preventDefault()}
+          onBlur={input.onBlur}
+          step={15}
+          onChange={input.onChange}
+          value={input.value || null}
+          placeholder={placeholder}
+          {...rest}
+          id="id"
+          includeTime={true}
+        />
+        {touched && !!error && (
+          <Label basic color="red">
+            {error}
+          </Label>
+        )}
+      </Form.Field>
+    </Localization>
   );
 };
 
