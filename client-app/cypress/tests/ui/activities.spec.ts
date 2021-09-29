3502e03d3a4e5cb1bfd2a662b29c681f352edc6f
MartinGrue <gruebler.martin@outlook.de>
2021-09-29 13:42:46 +0200

    basic e2e all specs setup

---
+++ client-app/cypress/tests/ui/activities.spec.ts
@@ -0,0 +1,6 @@
+//should display activities
+//filter should work
+//select date should work
+//maps should work
+//avatas loaded
+//information should be correct
---
+++ client-app/cypress/tests/ui/manage-activity.spec.ts
@@ -0,0 +1,5 @@
+//information should be displayed correct
+//delete should work
+//cancel should work
+//submit should work
+//check if refresh work
