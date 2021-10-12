13063e5534774d8489e2c7c9af75d4994e6fe16a
MartinGrue <gruebler.martin@outlook.de>
2021-10-12 17:16:20 +0200

    basic cypress in pipeline setup

--- deployment/test/startup.sh
+++ deployment/test/startup.sh
@@ -2,6 +2,7 @@
 # Start the first process
 (cd /e2e/client-app; nohup npm run start &)
 # Start the second process
-(cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/home.spec.ts )
+sleep 10
+(cd /e2e/client-app; npx cypress run )
 
 
