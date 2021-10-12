b7c2dc9dcdfb5ca64e3a1aa7f8f399633bab603e
MartinGrue <gruebler.martin@outlook.de>
2021-10-12 19:27:12 +0200

    test

--- deployment/test/startup.sh
+++ deployment/test/startup.sh
@@ -3,4 +3,4 @@
 (cd /e2e/client-app; nohup npm run start &)
 # Start the second process
 sleep 10
-(cd /e2e/client-app; npx cypress run )
+(cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/manage-activity.spec.ts )
