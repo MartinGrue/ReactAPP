0c6894494155a5570d08d5b856ed7005d30427d5
MartinGrue <gruebler.martin@outlook.de>
2021-10-12 19:02:01 +0200

    tests

--- client-app/cypress/support/commands.ts
+++ client-app/cypress/support/commands.ts
@@ -32,8 +32,7 @@ Cypress.Commands.add("login", (email, password) => {
   cy.get("[data-cy=login]").click();
   cy.get("[name=email]").clear().type(email);
   cy.get("[name=password]").clear().type(password);
-  cy.get("[data-cy=login-submit]").click();
-  cy.wait("@loginUser");
+  cy.get("[data-cy=login-submit]").click().wait("@loginUser");
   cy.wait("@userLoad");
   log.end();
 });
