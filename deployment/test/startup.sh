3ef9686a731dc052d8defed319ad2c011fa30ede
MartinGrue <gruebler.martin@outlook.de>
2021-10-13 21:42:04 +0200

    test

--- deployment/test/startup.sh
+++ deployment/test/startup.sh
@@ -8,30 +8,30 @@ sleep 10
 case $TEST in
 
   profile)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/profile.spec.ts --browser $BROWSER )
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/profile.spec.ts --browser $BRO )
     ;;
 
   auth)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/auth.spec.ts --browser $BROWSER  )
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/auth.spec.ts --browser $BRO  )
     ;;
 
   manage)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/manage-activity.spec.ts --browser $BROWSER )
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/manage-activity.spec.ts --browser $BRO )
     ;;
 
   activities)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/activities.spec.ts --browser $BROWSER )
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/activities.spec.ts --browser $BRO )
     ;;
 
   activity)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/activity.spec.ts --browser $BROWSER )
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/activity.spec.ts --browser $BRO )
     ;;
 
   createactivity)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/create-activity.spec.ts --browser $BROWSER )    
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/create-activity.spec.ts --browser $BRO )    
     ;;
 
   home)
-    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/home.spec.ts --browser $BROWSER )
+    (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/home.spec.ts --browser $BRO )
     ;;
 esac
