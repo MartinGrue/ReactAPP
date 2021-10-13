7431e0712435b62098d9eb606f72714737408547
MartinGrue <gruebler.martin@outlook.de>
2021-10-13 21:20:27 +0200

    s

--- deployment/test/startup.sh
+++ deployment/test/startup.sh
@@ -2,7 +2,7 @@
 # # Start the first process
 (cd /e2e/client-app; nohup npm run start &)
 # # Start the second process
-sleep 5
+sleep 10
 # (cd /e2e/client-app; npx cypress run --spec cypress/tests/ui/profile.spec.ts --spec cypress/tests/ui/manage-activity.spec.ts)
 
 case $TEST in
